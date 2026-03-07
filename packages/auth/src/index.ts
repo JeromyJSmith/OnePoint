8752a048800cefd02767a718bc9f7c9d19fe9e7a
Jeromy J Smith <JeromyJSmith@gmail.com>
2026-03-07 11:39:06 -0700

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,19 @@
+import { db } from "@WeCareALOT/db";
+import * as schema from "@WeCareALOT/db/schema/auth";
+import { env } from "@WeCareALOT/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [nextCookies()],
+});
